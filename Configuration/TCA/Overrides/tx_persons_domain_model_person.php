11ec46aa3916e2c11d6f7280976efbf579c518e7
Dirk Wenzel <wenzel@cps-it.de>
2017-05-29 17:35:38 +0200

    [BUGFIX] invalid configuration fixed: field department replaced by field status

--- Configuration/TCA/Overrides/tx_persons_domain_model_person.php
+++ Configuration/TCA/Overrides/tx_persons_domain_model_person.php
@@ -1,18 +1,17 @@
 <?php
 
+$emConfig = \CPSIT\Persons\Utility\EmConfigurationUtility::getConfiguration();
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::makeCategorizable(
     'persons',
     'tx_persons_domain_model_person',
-    'departments',
+    'status',
     [
-// Set a custom label
         'label' => $ll . 'tx_persons_domain_model_person.departments',
-// This field should not be an exclude-field
         'exclude' => true,
-// Override generic configuration, e.g. sort by title rather than by sorting
         'fieldConfiguration' => [
             'treeConfig' => [
-                'rootUid' => $emConfig->getDepartmentRootCategoryId(),
+                'rootUid' => $emConfig->getStatusRootCategoryId(),
                 'maxLevels' => 2,
                 'appearance' => [
                     'showHeader' => true,
@@ -21,9 +20,7 @@
             ],
             'foreign_table_where' => ' AND sys_category.sys_language_uid IN (-1, 0) ORDER BY sys_category.title ASC',
         ],
-// string (keyword), see TCA reference for details
         'l10n_mode' => 'exclude',
-// list of keywords, see TCA reference for details
         'l10n_display' => 'hideDiff',
     ]
 );
